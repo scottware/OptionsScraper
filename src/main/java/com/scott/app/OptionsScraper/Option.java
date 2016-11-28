1ab165906fba2037d491a9c810fee9fd1e249700
Scott Ware <sware@Scotts-MacBook-Air.local>
2016-11-27 16:49:27 -0800

    drill into monthlies and print PUT strikes

---
+++ src/main/java/com/scott/app/OptionsScraper/Option.java
@@ -0,0 +1,5 @@
+package com.scott.app.OptionsScraper;
+
+public class Option {
+
+}
--- src/main/java/com/scott/app/OptionsScraper/OptionsScraper.java
+++ src/main/java/com/scott/app/OptionsScraper/OptionsScraper.java
@@ -1,7 +1,7 @@
 package com.scott.app.OptionsScraper;
 
 public class OptionsScraper {
-	final boolean DEBUG = true;
+	final boolean DEBUG = false;
 
 	public static void main(String[] args) {
 		OptionsScraper OS = new OptionsScraper();
